32701c0f98514b0a6908ec273866e8eed944bc29
Balwant Singh <bsingh@Balwants-COG-MacBook-Pro.local>
2018-09-29 15:45:30 +1000

    exposed as a NPM Package

--- velixid.js
+++ velixid.js
@@ -3,34 +3,34 @@ function openPopupCenter(url, title, w, h) {
     // Most browsers use window.screenLeft
     // Firefox uses screen.left
     var dualScreenLeft = getFirstNumber(window.screenLeft, screen.left),
-      dualScreenTop = getFirstNumber(window.screenTop, screen.top),
-      width = getFirstNumber(window.innerWidth, document.documentElement.clientWidth, screen.width),
-      height = getFirstNumber(window.innerHeight, document.documentElement.clientHeight, screen.height),
-      left = ((width / 2) - (w / 2)) + dualScreenLeft,
-      top = ((height / 2) - (h / 2)) + dualScreenTop,
-      newWindow = window.open(url, title, getSpecs());
-  
+        dualScreenTop = getFirstNumber(window.screenTop, screen.top),
+        width = getFirstNumber(window.innerWidth, document.documentElement.clientWidth, screen.width),
+        height = getFirstNumber(window.innerHeight, document.documentElement.clientHeight, screen.height),
+        left = ((width / 2) - (w / 2)) + dualScreenLeft,
+        top = ((height / 2) - (h / 2)) + dualScreenTop,
+        newWindow = window.open(url, title, getSpecs());
+
     // Puts focus on the newWindow
     if (window.focus) {
-      newWindow.focus();
+        newWindow.focus();
     }
-  
+
     return newWindow;
-  
+
     function getSpecs() {
-      return 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left;
+        return 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left;
     }
-  
+
     function getFirstNumber() {
-      for(var i = 0, len = arguments.length; i < len; i++) {
-        var value = arguments[i];
-  
-        if (typeof value === 'number') {
-          return value;
+        for (var i = 0, len = arguments.length; i < len; i++) {
+            var value = arguments[i];
+
+            if (typeof value === 'number') {
+                return value;
+            }
         }
-      }
     }
-  }
+}
 
 
 var VelixID = {
@@ -42,7 +42,7 @@ var VelixID = {
             this._addBehaviour(button);
         }.bind(this));
 
-        window.addEventListener('message', function(e) {
+        window.addEventListener('message', function (e) {
             if (this._popup && e.data.close) {
                 if (e.data.data === 'cancel') {
                     this._popup.close();
@@ -59,7 +59,7 @@ var VelixID = {
                     }
                 }
             }
-        }.bind(this) , false);
+        }.bind(this), false);
     },
 
     _styleButton: function (button) {
@@ -74,7 +74,7 @@ var VelixID = {
         button.style.outline = 'none';
         button.style.cursor = 'pointer';
     },
-    
+
     _addBehaviour: function (button) {
         switch (button.dataset['buttonType']) {
             case 'velixid-login':
@@ -97,7 +97,7 @@ var VelixID = {
     },
 
     _fireEvent: function (event, data) {
-        if(this._events[event]) {
+        if (this._events[event]) {
             for (let index = 0; index < this._events[event].length; index++) {
                 let callback = this._events[event][index];
                 callback(data);
@@ -105,9 +105,11 @@ var VelixID = {
         }
     },
 
-    on: function(event, callback) {
-        if(this._events[event] && typeof callback === 'function') {
+    on: function (event, callback) {
+        if (this._events[event] && typeof callback === 'function') {
             this._events[event].push(callback);
         }
     }
 }
+
+export default VelixID;
